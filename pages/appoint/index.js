58207bd03ad41400a2e8b5916de38265a56c21a8
zhangchenchen <664694324@qq.com>
2018-05-13 16:03:29 +0800

    1

--- pages/appoint/index.js
+++ pages/appoint/index.js
@@ -1,27 +1,37 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+import {formatTime} from '../../utils/util.js';
 Page({
   data: {
-    productList: [{
-      src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/1.png?t=201859',
-      title: '猫咪',
-      subtitle: '好漂亮的猫咪呀',
-      price: '12'
-    },
-      {
-        src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/1.png?t=201859',
-        title: '猫咪',
-        subtitle: '好漂亮的猫咪呀',
-        price: '12'
-      }, {
-        src: 'https://mp.weixin.qq.com/debug/wxadoc/dev/image/cat/1.png?t=201859',
-        title: '猫咪',
-        subtitle: '好漂亮的猫咪呀',
-        price: '12'
-      }]
+    array: ['洗澡', '美容', '疫苗'],
+    index: 0,
+    date: '',
+    time: '12:01'
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
   },
   onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: '当前页面'
+    })
+    const today = formatTime(new Date())
+    this.setData({
+      date: today.split(' ')[0]
+    })
+    this.setData({
+      time: today.split(' ')[1].substr(0, 5)
+    })
   }
 })
